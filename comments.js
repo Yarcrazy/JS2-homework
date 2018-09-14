031934a6f85a02d39a36e989f338c0d22304f99f
Иван <yarcrazy@yandex.ru>
2018-09-14 21:58:55 +0700

    научил добавлять комменты, отображать комменты

--- comments.js
+++ comments.js
@@ -1,48 +1,96 @@
 "use strict";
 
-function Comments(id) {
-  this.id = id;
-}
+// function Comments(id) {
+//   this.id = id;
+// }
+//
+// Comments.prototype.renderNew = function(text) {
+//   var $ul = $('#comments');
+//   var $li = $('<li />', {
+//     text: text + '',
+//     id: this.id
+//   });
+//   console.log($li);
+//   var $btnApprove = $('<button />', {
+//     text: 'approve',
+//     class: 'btnApp'
+//   });
+//   var $btnDecline = $('<button />', {
+//     text: 'decline',
+//     class: 'btnApp'
+//   });
+//   $li.append($btnApprove, $btnDecline);
+//   $ul.append($li);
+//   $ul.data('last-id', this.id);
+// };
 
-Comments.prototype.renderNew = function(text) {
+function buildComments() {
   var $ul = $('#comments');
-  var $li = $('<li />', {
-    text: text + '',
-    id: this.id,
-  });
-  var $btnApprove = $('<button />', {
-    text: 'approve',
-    class: 'btnApp',
-  });
-  var $btnDecline = $('<button />', {
-    text: 'decline',
-    class: 'btnApp',
-  });
-  $li.append($btnApprove, $btnDecline);
-  $ul.append($li);
-  $ul.data('last-id', this.id);
-};
+  // Очищаем комментарии
+  $ul.empty();
+  // Отправляем запрос на получение списка комментариев
+  $.ajax({
+    url: 'http://localhost:3000/comments',
+    dataType: 'json',
+    success: function(comments) {
+      // Перебираем комментарии
+      comments.forEach(function(item) {
+        // Создаем комментарий в списке
+        var $li = $('<li />', {
+          text: item.text
+        });
+
+        // Создаем кнопки одобрить и отклонить
+        var $btnApprove = $('<button />', {
+          text: 'approve',
+          'data-id': item.id
+        });
+        var $btnDecline = $('<button />', {
+          text: 'decline',
+          'data-id': item.id
+        });
+
+        // Добавляем все в dom
+        $li.append($btnApprove);
+        $li.append($btnDecline);
+        $ul.append($li);
+      });
+      $ul.attr('data-last-id', comments.length);
+    }
+  })
+}
 
 (function($) {
   $(function() {
+    buildComments();
     // При нажатии на кнопку отправить добавляем комментарий в json и отрисовываем
     $('#send').on('click', function() {
-      var currentComm = +$('#comments').data('last-id') + 1;
-      var text = $('#textarea').text();
+      var nextComm = +$('#comments').attr('data-last-id') + 1;
+      var text = $('#textarea').val();
       $.ajax({
         url: 'http://localhost:3000/comments/',
         type: 'POST',
         headers: {
-          'content-type': 'application/json',
+          'content-type': 'application/json'
         },
         data: JSON.stringify({
-          id: currentComm,
-          text: text,
+          id: nextComm,
+          text: text
         }),
+        success: function() {
+          buildComments();
+        }
       });
-      var comment = new Comments(currentComm);
-      comment.renderNew(text);
-      console.log($('#textarea').text());
     });
+
+    $('#clear').on('click', function() {
+      $.ajax({
+        url: 'http://localhost:3000/comments',
+        type: 'DELETE',
+        success: function() {
+          buildComments();
+        }
+      })
+    })
   })
 })(jQuery);
