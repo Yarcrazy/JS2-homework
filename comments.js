d1fb524d6df92828329812e16771c1af0d9dcaa1
Иван <yarcrazy@yandex.ru>
2018-09-15 16:06:49 +0700

    Доделал комментарии, но есть одна проблема: когда удаляешь какой-нибудь коммент из середины, пытаешься создать следующий, но id в json не может создать одинаковый

--- comments.js
+++ comments.js
@@ -37,22 +37,31 @@ function buildComments() {
       comments.forEach(function(item) {
         // Создаем комментарий в списке
         var $li = $('<li />', {
-          text: item.text
+          text: item.text,
+          class: item.status
         });
 
-        // Создаем кнопки одобрить и отклонить
-        var $btnApprove = $('<button />', {
-          text: 'approve',
-          'data-id': item.id
-        });
-        var $btnDecline = $('<button />', {
-          text: 'decline',
-          'data-id': item.id
-        });
+        // Создаем кнопки одобрить и отклонить, если у комментария статус отсутствует (undefined)
+        if (item.status === undefined) {
+          var $btnApprove = $('<button />', {
+            text: 'approve', 'data-id': item.id, class: 'grade'
+          });
+          var $btnDecline = $('<button />', {
+            text: 'decline', 'data-id': item.id, class: 'grade'
+          });
 
-        // Добавляем все в dom
-        $li.append($btnApprove);
-        $li.append($btnDecline);
+          // Добавляем кнопки в li
+          $li.append($btnApprove);
+          $li.append($btnDecline);
+        }
+
+        // Создаем и добавляем кнопку очистить к каждому комментарию
+        var $btnClear = $('<button />', {
+          text: 'clear', 'data-id': item.id, class: 'clear'
+        });
+        $li.append($btnClear);
+
+        // Добавляем li к ul
         $ul.append($li);
       });
       $ul.attr('data-last-id', comments.length);
@@ -63,6 +72,7 @@ function buildComments() {
 (function($) {
   $(function() {
     buildComments();
+
     // При нажатии на кнопку отправить добавляем комментарий в json и отрисовываем
     $('#send').on('click', function() {
       var nextComm = +$('#comments').attr('data-last-id') + 1;
@@ -83,14 +93,35 @@ function buildComments() {
       });
     });
 
-    $('#clear').on('click', function() {
+    // При нажатии на Clear удаляем комментарий из json
+    $('#comments').on('click', '.clear', function() {
+      var id = $(this).attr('data-id');
       $.ajax({
-        url: 'http://localhost:3000/comments',
+        url: 'http://localhost:3000/comments/' + id,
         type: 'DELETE',
         success: function() {
           buildComments();
         }
       })
-    })
+    });
+
+    // При нажатии на кнопку оценки добавляем в json статус одобрено или отклонено и отрисовываем комментарии
+    $('#comments').on('click', '.grade', function() {
+      var id = $(this).attr('data-id');
+      var grade = $(this).text();
+      $.ajax({
+        url: 'http://localhost:3000/comments/' + id,
+        type: 'PATCH',
+        headers: {
+          'content-type': 'application/json'
+        },
+        data: JSON.stringify({
+          status: grade
+        }),
+        success: function() {
+          buildComments();
+        }
+      })
+    });
   })
 })(jQuery);
