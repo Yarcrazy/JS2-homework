8c9a52d07694889071ca200c178d320d15a6e0aa
Yarcrazy <yacrazy@yandex.ru>
2018-09-17 22:47:57 +0700

    Сделал L4 HW 2

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 "use strict";
 
 function builtCities() {
-  var $select = $('<select />');
   $.ajax({
     "url": "http://localhost:3000/cities",
     "dataType": "json",
-    success: function() {
-
+    success: function(cities) {
+      var $select = $('<select />', {
+        id: 'cities'
+      });
+      cities.forEach(function(city) {
+        var $option = $('<option />', {
+          text: city.name
+        });
+        $select.append($option);
+      });
+      $('#cityLabel').append($select);
     }
 });
 }
+
+(function($) {
+  $(function() {
+    builtCities();
+  })
+})(jQuery);
